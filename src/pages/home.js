0752b3e1e1fbefa009832ff1df5506c61a3d2988
umarjabbar <umarjuabbar@gmail.com>
2020-09-12 23:45:35 +0700

    Tampil data home (class component)

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,51 +1,57 @@
-import React, {Component} from 'react'
-
+import React, {Component, useEffect, useState} from 'react'
+import axios from 'axios'
 
 class Home extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      movies: []
+    }
+  }
+  
+  componentDidMount() {
+    axios.get(`http://backendexample.sanbercloud.com/api/movies`)
+    .then(res => {
+      const movies = res.data;
+      this.setState({ movies });
+    })
+  }
   render() {
     return (
       <>
-        <section>
-          <h1>Best Movies List</h1>
-          <div id="article-list">
-            <div class="article">
-              <a href="#"><h3>Lorem Post 1</h3></a>
-              <p>
-                Lorem Ipsum Dolor Sit Amet, mea te verear signiferumque, per illum labores ne. Blandit omnesque scripserit pri ex, et pri dicant eirmod deserunt. Aeque perpetua ea nec. Sit erant patrioque delicatissimi ut. Et sea quem sint, nam in minim voluptatibus. Etiam placerat eam in.
-              </p>
-            </div>
-            <div class="article">
-              <a href="#"><h3>Lorem Post 2</h3></a>
-              <p>
-                Lorem Ipsum Dolor Sit Amet, mea te verear signiferumque, per illum labores ne. Blandit omnesque scripserit pri ex, et pri dicant eirmod deserunt. Aeque perpetua ea nec. Sit erant patrioque delicatissimi ut. Et sea quem sint, nam in minim voluptatibus. Etiam placerat eam in.
-              </p>
-            </div>
-            <div class="article">
-              <a href="#"><h3>Lorem Post 3</h3></a>
-              <p>
-                Lorem Ipsum Dolor Sit Amet, mea te verear signiferumque, per illum labores ne. Blandit omnesque scripserit pri ex, et pri dicant eirmod deserunt. Aeque perpetua ea nec. Sit erant patrioque delicatissimi ut. Et sea quem sint, nam in minim voluptatibus. Etiam placerat eam in.
-              </p>
-            </div>
-            <div class="article">
-              <a href="#"><h3>Lorem Post 4</h3></a>
-              <p>
-                Lorem Ipsum Dolor Sit Amet, mea te verear signiferumque, per illum labores ne. Blandit omnesque scripserit pri ex, et pri dicant eirmod deserunt. Aeque perpetua ea nec. Sit erant patrioque delicatissimi ut. Et sea quem sint, nam in minim voluptatibus. Etiam placerat eam in.
-              </p>
-            </div>
-            <div class="article">
-              <a href="#"><h3>Lorem Post 5</h3></a>
-              <p>
-                Lorem Ipsum Dolor Sit Amet, mea te verear signiferumque, per illum labores ne. Blandit omnesque scripserit pri ex, et pri dicant eirmod deserunt. Aeque perpetua ea nec. Sit erant patrioque delicatissimi ut. Et sea quem sint, nam in minim voluptatibus. Etiam placerat eam in.
-              </p>
-            </div>
-          </div>
-        </section>
-        <footer>
-          <h5>copyright &copy; 2020 by Sanbercode</h5>
-        </footer>
-      </>
+      <section>
+        <h1>Best Movies List</h1>
+        <div id="article-list">
+          {
+           this.state.movies.map(movie => {
+            const movieThumbnail = {
+              backgroundImage: 'url(' + movie.image_url + ')'
+            }
+            console.log(this.state.movies)
+            return(
+              <div class="article">
+                <div className="movie-info">
+                  <div style={movieThumbnail} className="movie-thumbnail"></div>
+                  <div className="info">
+                    <a href="#"><h3>{movie.title}</h3></a>
+                    <span>Rating: {movie.rating}</span>
+                    <span>Duration: {movie.duration} Hours</span>
+                    <span>Genre: {movie.genre}</span>
+                  </div>
+                </div>
+                <p>{movie.description}</p>
+              </div>
+            )})
+          }
+        </div>
+      </section>
+      <footer>
+        <h5>copyright &copy; 2020 by Sanbercode</h5>
+      </footer>
+    </>
     )
   }
 }
+
 
 export default Home
